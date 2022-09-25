0f482222b403aefb5d7605313fde8b1be189468c
kuehbiko <88494428+kuehbiko@users.noreply.github.com>
2022-09-25 23:27:07 +0800

    Create SQL Practice Problems by Sylvia Moestl Vasilik.sql

---
+++ SQL/SQL Practice Problems by Sylvia Moestl Vasilik.sql
@@ -0,0 +1,10 @@
+SQL Practice Problems
+57 beginning, intermediate, and advanced challenges for you to solve using a "learn-by-doing" approach
+by Sylvia Moestl Vasilik
+
+Introductory Problems
+
+1. Which shippers do we have?
+We have a table called Shippers. Return all the fields from all the shippers
+
+SELECT * FROM shippers
