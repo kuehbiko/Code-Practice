0674b660bc8414d7cdded528522dda1ec6d25550
kuehbiko <88494428+kuehbiko@users.noreply.github.com>
2022-10-05 13:18:13 +0800

    Create Lesson 10: Queries with aggregates (Pt. 1).sql

---
+++ SQL/SQLBolt - Introduction to SQL/Lesson 10: Queries with aggregates (Pt. 1).sql
@@ -0,0 +1,4 @@
+-- SQL Lesson 10: Queries with aggregates (Pt. 1)
+-- Find the longest time that an employee has been at the studio
+-- For each role, find the average number of years employed by employees in that role
+-- Find the total number of employee years worked in each building
