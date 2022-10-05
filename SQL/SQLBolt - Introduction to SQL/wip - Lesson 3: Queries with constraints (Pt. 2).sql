a9e457bb7dcbd89307468b40f8cc8401108a27f3
kuehbiko <88494428+kuehbiko@users.noreply.github.com>
2022-10-05 11:16:52 +0800

    Create wip - Lesson 3: Queries with constraints (Pt. 2).sql

---
+++ SQL/SQLBolt - Introduction to SQL/wip - Lesson 3: Queries with constraints (Pt. 2).sql
@@ -0,0 +1,9 @@
+-- SQL Lesson 3: Queries with constraints (Pt. 2)
+
+-- Find all the Toy Story movies
+
+-- Find all the movies directed by John Lasseter
+
+-- Find all the movies (and director) not directed by John Lasseter
+
+-- Find all the WALL-* movies
