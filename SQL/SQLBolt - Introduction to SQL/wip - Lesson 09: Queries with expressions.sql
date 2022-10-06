382fcb820efee4aa883029baf4577bafaf2af6a9
E. Lim <88494428+kuehbiko@users.noreply.github.com>
2022-10-06 16:04:19 +0800

    Update and rename wip - Lesson 09: Queries with expressions.sql to Lesson 09: Queries with expressions.sql

--- SQL/SQLBolt - Introduction to SQL/wip - Lesson 09: Queries with expressions.sql
+++
@@ -1,4 +0,0 @@
--- SQL Lesson 9: Queries with expressions
--- List all movies and their combined sales in millions of dollars
--- List all movies and their ratings in percent
--- List all movies that were released on even number years
